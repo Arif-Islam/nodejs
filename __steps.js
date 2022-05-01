045b4e2444d2d47b5285389db923c14143f3bed2
Arif <iariful950@gmail.com>
2022-05-01 19:41:52 +0600

    adding steps

--- __steps.js
+++ __steps.js
@@ -1,9 +1,21 @@
 /* 
+    ----------------------------------------
+    Basic Server Setup
     1. create a folder (manually or mkdir)
     2. open commnad line to that folder
     3. npm init or npm init -y
     4. npm install express
     5. create index.js
+
+    -----------------------------------------
+    GET
+    -----------------------------------------
+    1. install cors, use cors as middleware
+
+    -----------------------------------------
+    POST
+    -----------------------------------------
+    1. use express as middleware
     -----------------------------------------
     For Express/Node Server
     -----------------------------------------
@@ -14,4 +26,54 @@
     5. listen to port
     6. node index.js
     7. check browser for that port
+
+    -----------------------------------------
+    1. Create a node server with 5 steps
+    -----------------------------------------
+    1.1. create folder
+    1.2. npm init
+    1.3. npm i express cors mongodb
+    1.4. script-dev: nodemon index.js
+    1.5. create index.js
+    1.6. use 5 steps to create a node server
+
+    -----------------------------------------
+    Create Atlas Account
+    -----------------------------------------
+    1. sign up with google
+    2. create cluster
+    3. create user arif and password
+    4. Network Acces -> ip address: allow all
+    5. database > connect > code copy paste in index.js
+
+    -----------------------------------------
+    CRUP Operations
+    -----------------------------------------
+    1. node mongodb crud > fundamentals
+    2. create async run function
+
+    ------------------------------------------------------
+    Integrate sending data from client side to server side
+    ------------------------------------------------------
+    1. client side - create form
+    2. onSubmit get form data and create user object
+    3. on Server : create user POST method to receive data on the backend
+    4. on client side: set fetch with POST, headers, body
+    5. make sure you return a json from the POST API
+
+    ------------------------------------
+    Load data to the client side
+    ------------------------------------
+    1. create get API on the server side
+    2. create a query object 
+    3. collection.find(query)
+    4. cursor.toArray()
+    5. return result
+    6. use useEffect as before to load data and show it in the UI
+
+    -----------------------------------
+    Delete data 
+    -----------------------------------
+    
+
 */
--- index.js
+++ index.js
@@ -1,10 +1,79 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// user: arif
+// password: m4KEzLs4RczfBvd5
+
+const uri = "mongodb+srv://arif:m4KEzLs4RczfBvd5@cluster0.djuxn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("users");
+
+        // get user
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+
+        app.get('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        })
+
+        // update user
+        app.put('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updatedUser.name,
+                    email: updatedUser.email
+                },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
+
+        // POST user: add new user from client side information
+        app.post('/user', async (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        // delete user
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Long Way Ahead!");
@@ -21,7 +90,7 @@ const users = [
 
 app.get('/users', (req, res) => {
     // filter by search query parameter
-    if (req.query.name) {   
+    if (req.query.name) {
         const search = req.query.name.toLowerCase();
         const matched = users.filter(user => user.name.toLowerCase().includes(search));
         res.send(matched);
@@ -33,19 +102,19 @@ app.get('/users', (req, res) => {
 
 });
 
-app.get('/user/:id', (req, res) => {
-    console.log(req.params);
-    const id = parseInt(req.params.id);
-    const user = users.find(u => u.id === id);
-    res.send(user);
-});
+// app.get('/user/:id', (req, res) => {
+//     console.log(req.params);
+//     const id = parseInt(req.params.id);
+//     const user = users.find(u => u.id === id);
+//     res.send(user);
+// });
 
-app.post('/user', (req, res) => {
-    console.log('request', req.body);
-    const user = req.body;
-    user.id = users.length + 1;
-    res.send(user);
-})
+// app.post('/user', (req, res) => {
+//     console.log('request', req.body);
+//     const user = req.body;
+//     user.id = users.length + 1;
+//     res.send(user);
+// })
 
 app.listen(port, () => {
     console.log("Listening to port ", port);
